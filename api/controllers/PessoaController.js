07b652919a709221fcbe641ad0d51f0420062267
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2023-07-20 07:34:40 -0300

    Aula 4

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -188,8 +188,19 @@ class PessoaController {
         having: Sequelize.literal(`count (turma_id) >= ${lotacaoTurma}`)
       })
       return res.status(200).json(turmasLotadas)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
 
-
+  static async cancelaPessoa(req, res) {
+    const { estudanteId } = req.params
+    try {
+      database.sequelize.transaction(async transacao => {
+        await database.Pessoas.update({ ativo: false},{where: {id: Number(estudanteId)}}, {transaction: transacao})
+        await database.Matriculas.update({status: 'cancelado'}, {where: {estudante_id: Number(estudanteId)}}, {transaction: transacao})
+        return res.status(200).json({message: `matriculas ref. estudante ${estudanteId} cancelado`})
+      })
     } catch (error) {
       return res.status(500).json(error.message)
     }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -13,14 +13,12 @@ router
   .get('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.pegaUmaMatricula)
   .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
   .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
+  .post('/pessoas/:estudanteId/cancela', PessoaController.cancelaPessoa)
   .put('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.atualizaMatricula)
   .delete('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.apagaMatricula)
   .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
   .get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatricula)
   .get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.pegaMatriculasPorTurma)
   .get('/pessoas/matricula/lotada', PessoaController.pegaMatriculasLotadas)
-
-
-
 
 module.exports = router
